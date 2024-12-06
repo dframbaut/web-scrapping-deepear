3e3d3fe7ab4d7f6677f189687e11cd8b6b5a2441
untitled99941 <147786707+untitled99941@users.noreply.github.com>
2024-12-05 19:14:30 -0500

    Update simi.py

--- Local_col/simi.py
+++ Local_col/simi.py
@@ -7,7 +7,7 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 # Buscar la tabla
 table = soup.find("table")
-
+#Creacion de listas para guardar datos de cada celda
 Nombres=[]
 Fechas=[]
 Epigrafe=[]
@@ -18,18 +18,18 @@ if tbody:
     for row_index, row in enumerate(tbody.find_all("tr"), start=1):
         for col_index, cell in enumerate(row.find_all("td"), start=1):
             cell_content = cell.get_text(strip=True)    
-            # Lógica personalizada para cada columna
-            if col_index == 1:  # Primera columna (por ejemplo, nombres)
+            #Cada columna debe ser un dato diferente 
+            if col_index == 1:  #N° de los acuerdos
                 Nombres.append(cell_content)  # Convertir en mayúsculas
-            elif col_index == 2:  # Segunda columna (por ejemplo, edades)
+            elif col_index == 2:  #N° proyecto de acuerdo
                 Fechas.append(cell_content)
-            elif col_index == 3:  # Tercera columna (por ejemplo, ciudades)
+            elif col_index == 3:  #Epigrafe
                 Epigrafe.append(cell_content)
-            elif col_index == 4:  # Tercera columna (por ejemplo, ciudades)
-                Tipo.append(cell_content)  # Capitalizar
-            elif col_index == 5:  # Tercera columna (por ejemplo, ciudades)
+            elif col_index == 4:  #Tipo o estado
+                Tipo.append(cell_content) 
+            elif col_index == 5:  #botones ¿cual es el link?
                 botones.append(cell_content)  # Capitalizar
-
+#Se imprime cada una de las listas creadas
 print(Nombres)
 print(Fechas)
 print(Epigrafe)
