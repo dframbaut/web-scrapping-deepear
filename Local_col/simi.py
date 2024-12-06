defb6640cd4ca5c99b3b4c132b64d772ea870d30
untitled99941 <147786707+untitled99941@users.noreply.github.com>
2024-12-06 08:56:52 -0500

    Update simi.py

--- Local_col/simi.py
+++ Local_col/simi.py
@@ -7,28 +7,29 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 # Buscar la tabla
 table = soup.find("table")
-
+#Creacion de listas
 Nombres=[]
 Fechas=[]
 Epigrafe=[]
 Tipo=[]
 botones=[]
 tbody = table.find("tbody")
+#Recorrer y añadir celdas a cada lista
 if tbody:
     for row_index, row in enumerate(tbody.find_all("tr"), start=1):
         for col_index, cell in enumerate(row.find_all("td"), start=1):
             cell_content = cell.get_text(strip=True)    
             if col_index == 1: Nombre
-                Nombres.append(cell_content)  # Convertir en mayúsculas
+                Nombres.append(cell_content)
             elif col_index == 2:Fecha
                 Fechas.append(cell_content)
             elif col_index == 3:Epigrafe
                 Epigrafe.append(cell_content)
             elif col_index == 4: Tipo
-                Tipo.append(cell_content)  # Capitalizar
+                Tipo.append(cell_content)
             elif col_index == 5: botones
-                botones.append(cell_content)  # Capitalizar
-
+                botones.append(cell_content)
+#Imprimir cada una de las listas
 print(Nombres)
 print(Fechas)
 print(Epigrafe)
