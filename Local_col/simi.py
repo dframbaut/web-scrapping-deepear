38e7954ce0a6f880f8354874b2ebf4a15d15a8cd
untitled99941 <147786707+untitled99941@users.noreply.github.com>
2024-12-06 08:55:48 -0500

    Update simi.py

--- Local_col/simi.py
+++ Local_col/simi.py
@@ -18,16 +18,15 @@ if tbody:
     for row_index, row in enumerate(tbody.find_all("tr"), start=1):
         for col_index, cell in enumerate(row.find_all("td"), start=1):
             cell_content = cell.get_text(strip=True)    
-            # Lógica personalizada para cada columna
-            if col_index == 1:  # Primera columna (por ejemplo, nombres)
+            if col_index == 1: Nombre
                 Nombres.append(cell_content)  # Convertir en mayúsculas
-            elif col_index == 2:  # Segunda columna (por ejemplo, edades)
+            elif col_index == 2:Fecha
                 Fechas.append(cell_content)
-            elif col_index == 3:  # Tercera columna (por ejemplo, ciudades)
+            elif col_index == 3:Epigrafe
                 Epigrafe.append(cell_content)
-            elif col_index == 4:  # Tercera columna (por ejemplo, ciudades)
+            elif col_index == 4: Tipo
                 Tipo.append(cell_content)  # Capitalizar
-            elif col_index == 5:  # Tercera columna (por ejemplo, ciudades)
+            elif col_index == 5: botones
                 botones.append(cell_content)  # Capitalizar
 
 print(Nombres)
